da36ab16725973c2ccdb4c7ed3d166327042d397
Alex Berumen <alexberumen@Alexs-MacBook-Pro.local>
2021-02-16 17:36:40 -0800

    started login/register proj

---
+++ src/components/SubmitForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+
+
+
+const SubmitForm = () => {
+    return (
+        <div>
+            yerr
+        </div>
+    )
+}
+
+export default SubmitForm
